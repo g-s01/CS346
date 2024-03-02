90ac9da6e4e165fc92d6b3454dcbc8e02a1b4fed
Gautam Juneja <g.juneja@iitg.ac.in>
2024-03-03 01:10:50 +0530

    pwd strength checker done

--- Assignment-2/Library_Management_System/Library_Management_System/registerPage.Designer.vb
+++ Assignment-2/Library_Management_System/Library_Management_System/registerPage.Designer.vb
@@ -32,6 +32,7 @@ Partial Class registerPage
         Me.Login = New System.Windows.Forms.LinkLabel()
         Me.TextBox1 = New System.Windows.Forms.TextBox()
         Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
         'Student
@@ -44,10 +45,10 @@ Partial Class registerPage
         Me.Student.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(CType(CType(165, Byte), Integer), CType(CType(113, Byte), Integer), CType(CType(224, Byte), Integer))
         Me.Student.FlatStyle = System.Windows.Forms.FlatStyle.Flat
         Me.Student.Font = New System.Drawing.Font("Segoe UI", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Student.Location = New System.Drawing.Point(307, 198)
-        Me.Student.Margin = New System.Windows.Forms.Padding(2)
+        Me.Student.Location = New System.Drawing.Point(409, 244)
+        Me.Student.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
         Me.Student.Name = "Student"
-        Me.Student.Size = New System.Drawing.Size(141, 29)
+        Me.Student.Size = New System.Drawing.Size(188, 36)
         Me.Student.TabIndex = 1
         Me.Student.Text = "Student"
         Me.Student.UseVisualStyleBackColor = False
@@ -62,10 +63,10 @@ Partial Class registerPage
         Me.Faculty.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(CType(CType(165, Byte), Integer), CType(CType(113, Byte), Integer), CType(CType(224, Byte), Integer))
         Me.Faculty.FlatStyle = System.Windows.Forms.FlatStyle.Flat
         Me.Faculty.Font = New System.Drawing.Font("Segoe UI", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Faculty.Location = New System.Drawing.Point(452, 198)
-        Me.Faculty.Margin = New System.Windows.Forms.Padding(2)
+        Me.Faculty.Location = New System.Drawing.Point(603, 244)
+        Me.Faculty.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
         Me.Faculty.Name = "Faculty"
-        Me.Faculty.Size = New System.Drawing.Size(128, 29)
+        Me.Faculty.Size = New System.Drawing.Size(171, 36)
         Me.Faculty.TabIndex = 2
         Me.Faculty.Text = "Faculty"
         Me.Faculty.UseVisualStyleBackColor = False
@@ -76,10 +77,10 @@ Partial Class registerPage
         Me.Username.BorderStyle = System.Windows.Forms.BorderStyle.None
         Me.Username.Font = New System.Drawing.Font("Segoe UI", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Username.ForeColor = System.Drawing.Color.Gray
-        Me.Username.Location = New System.Drawing.Point(341, 316)
-        Me.Username.Margin = New System.Windows.Forms.Padding(2)
+        Me.Username.Location = New System.Drawing.Point(455, 389)
+        Me.Username.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
         Me.Username.Name = "Username"
-        Me.Username.Size = New System.Drawing.Size(198, 19)
+        Me.Username.Size = New System.Drawing.Size(264, 23)
         Me.Username.TabIndex = 3
         Me.Username.Text = "Username"
         '
@@ -89,10 +90,10 @@ Partial Class registerPage
         Me.Password.BorderStyle = System.Windows.Forms.BorderStyle.None
         Me.Password.Font = New System.Drawing.Font("Segoe UI", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Password.ForeColor = System.Drawing.Color.Gray
-        Me.Password.Location = New System.Drawing.Point(341, 365)
-        Me.Password.Margin = New System.Windows.Forms.Padding(2)
+        Me.Password.Location = New System.Drawing.Point(455, 449)
+        Me.Password.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
         Me.Password.Name = "Password"
-        Me.Password.Size = New System.Drawing.Size(198, 19)
+        Me.Password.Size = New System.Drawing.Size(264, 23)
         Me.Password.TabIndex = 4
         Me.Password.Text = "Password"
         '
@@ -104,10 +105,10 @@ Partial Class registerPage
         Me.Registerbtn.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(CType(CType(165, Byte), Integer), CType(CType(113, Byte), Integer), CType(CType(224, Byte), Integer))
         Me.Registerbtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat
         Me.Registerbtn.Font = New System.Drawing.Font("Segoe UI", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Registerbtn.Location = New System.Drawing.Point(332, 459)
-        Me.Registerbtn.Margin = New System.Windows.Forms.Padding(2)
+        Me.Registerbtn.Location = New System.Drawing.Point(443, 565)
+        Me.Registerbtn.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
         Me.Registerbtn.Name = "Registerbtn"
-        Me.Registerbtn.Size = New System.Drawing.Size(224, 32)
+        Me.Registerbtn.Size = New System.Drawing.Size(299, 39)
         Me.Registerbtn.TabIndex = 5
         Me.Registerbtn.Text = "Register"
         Me.Registerbtn.UseVisualStyleBackColor = False
@@ -118,10 +119,10 @@ Partial Class registerPage
         Me.Uname.BorderStyle = System.Windows.Forms.BorderStyle.None
         Me.Uname.Font = New System.Drawing.Font("Segoe UI", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Uname.ForeColor = System.Drawing.Color.Gray
-        Me.Uname.Location = New System.Drawing.Point(341, 266)
-        Me.Uname.Margin = New System.Windows.Forms.Padding(2)
+        Me.Uname.Location = New System.Drawing.Point(455, 327)
+        Me.Uname.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
         Me.Uname.Name = "Uname"
-        Me.Uname.Size = New System.Drawing.Size(198, 19)
+        Me.Uname.Size = New System.Drawing.Size(264, 23)
         Me.Uname.TabIndex = 6
         Me.Uname.Text = "Name"
         '
@@ -131,10 +132,10 @@ Partial Class registerPage
         Me.CPassword.BorderStyle = System.Windows.Forms.BorderStyle.None
         Me.CPassword.Font = New System.Drawing.Font("Segoe UI", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.CPassword.ForeColor = System.Drawing.Color.Gray
-        Me.CPassword.Location = New System.Drawing.Point(341, 415)
-        Me.CPassword.Margin = New System.Windows.Forms.Padding(2)
+        Me.CPassword.Location = New System.Drawing.Point(455, 511)
+        Me.CPassword.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
         Me.CPassword.Name = "CPassword"
-        Me.CPassword.Size = New System.Drawing.Size(198, 19)
+        Me.CPassword.Size = New System.Drawing.Size(264, 23)
         Me.CPassword.TabIndex = 7
         Me.CPassword.Text = "Confirm Password"
         '
@@ -147,10 +148,9 @@ Partial Class registerPage
         Me.Login.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Login.LinkBehavior = System.Windows.Forms.LinkBehavior.NeverUnderline
         Me.Login.LinkColor = System.Drawing.Color.Black
-        Me.Login.Location = New System.Drawing.Point(768, 32)
-        Me.Login.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Login.Location = New System.Drawing.Point(1024, 39)
         Me.Login.Name = "Login"
-        Me.Login.Size = New System.Drawing.Size(53, 21)
+        Me.Login.Size = New System.Drawing.Size(64, 28)
         Me.Login.TabIndex = 8
         Me.Login.TabStop = True
         Me.Login.Text = "Login"
@@ -158,28 +158,40 @@ Partial Class registerPage
         '
         'TextBox1
         '
-        Me.TextBox1.Location = New System.Drawing.Point(677, 147)
+        Me.TextBox1.Location = New System.Drawing.Point(903, 181)
+        Me.TextBox1.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox1.Size = New System.Drawing.Size(132, 22)
         Me.TextBox1.TabIndex = 9
         Me.TextBox1.Text = "Enter your OTP"
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(687, 184)
+        Me.Button1.Location = New System.Drawing.Point(916, 226)
+        Me.Button1.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.Size = New System.Drawing.Size(100, 28)
         Me.Button1.TabIndex = 10
         Me.Button1.Text = "Check OTP"
         Me.Button1.UseVisualStyleBackColor = True
         '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(817, 291)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(51, 17)
+        Me.Label1.TabIndex = 11
+        Me.Label1.Text = "Label1"
+        '
         'registerPage
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackgroundImage = Global.Library_Management_System.My.Resources.Resources.registerBg
         Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
-        Me.ClientSize = New System.Drawing.Size(888, 578)
+        Me.ClientSize = New System.Drawing.Size(1181, 702)
+        Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.TextBox1)
         Me.Controls.Add(Me.Login)
@@ -191,9 +203,9 @@ Partial Class registerPage
         Me.Controls.Add(Me.Faculty)
         Me.Controls.Add(Me.Student)
         Me.DoubleBuffered = True
-        Me.Margin = New System.Windows.Forms.Padding(2)
-        Me.MaximumSize = New System.Drawing.Size(904, 617)
-        Me.MinimumSize = New System.Drawing.Size(904, 617)
+        Me.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.MaximumSize = New System.Drawing.Size(1199, 749)
+        Me.MinimumSize = New System.Drawing.Size(1199, 749)
         Me.Name = "registerPage"
         Me.Text = "registerPage"
         Me.ResumeLayout(False)
@@ -210,5 +222,6 @@ Partial Class registerPage
     Friend WithEvents Login As System.Windows.Forms.LinkLabel
     Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
     Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
 
 End Class
